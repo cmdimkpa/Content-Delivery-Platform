b5cf82a537371873fb770cd0763c8afb291ebca0
Monty Dimkpa <cmdimkpa@gmail.com>
2019-11-27 12:24:56 +0100

    Update website.py

--- website.py
+++ website.py
@@ -6,25 +6,6 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-global BASE_HTML
-
-BASE_HTML = """
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>AXA Current State Architecture</title>
-    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.js" charset="utf-8"></script>
-  </head>
-  <body>
-    __NAVBAR__
-  </body>
-</html>
-
-"""
-
 def responsify(status,message,data={}):
     code = int(status)
     a_dict = {"data":data,"message":message,"code":code}
@@ -41,6 +22,13 @@ def get_file(filename):
         return "<h1> Error Serving File: %s <h1>" % filename
 
 def build_navbar():
+    global NAVBAR
+    NAVBAR = ""
+    def add_entry(entry):
+        global NAVBAR
+        title = entry["unit"]; url = entry["url"]
+        NAVBAR+='<li class="list-group-item list-group-item-action bg-light" onclick="loadPage(`%s`)">%s</li>' % ("%s,%s"%(url,title),title)
+        return None
     url = "http://3.130.5.83:9271/ods/fetch_records"
     payload = {
     	"tablename":"ContentModel",
@@ -48,25 +36,13 @@ def build_navbar():
     }
     HEADERS = {"Content-Type":"application/json"}
     data = http.post(url,json.dumps(payload),headers=HEADERS).json()["data"]
-    BASE_NAV = """
-
-    """
-    return BASE_NAV
-
-def build_html():
-    global BASE_HTML
-    BASE_HTML = BASE_HTML.replace("__NAVBAR__",build_navbar())
-    return BASE_HTML
-
-def new_site(html):
-    homepage = "/home/ubuntu/website/templates/site.html"
-    p = open(homepage,"wb+"); p.write(html); p.close()
-    return None
+    build = map(add_entry,data)
+    return NAVBAR,data[0]["unit"]
 
 @app.route("/AXA-current-state-architecture")
 def load_website():
-    new_site(build_html())
-    return render_template("site.html")
+    NAVBAR,ACTIVE_LABEL = build_navbar()
+    return render_template("index.html",NAVBAR=NAVBAR,ACTIVE_LABEL=ACTIVE_LABEL)
 
 if __name__ == "__main__":
   app.run(host="172.31.29.36",port=2357,threaded=True)
